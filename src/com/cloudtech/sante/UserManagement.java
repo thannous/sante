8ccd96d08fd0ff8fb2832b351d1c1b44391f1e20
talanta <a.talviy@gmail.com>
2013-07-27 17:21:09 +0200

    connexion de la vue gestion utilisateurs a la popup d ajout

--- src/com/cloudtech/sante/UserManagement.java
+++ src/com/cloudtech/sante/UserManagement.java
@@ -1,8 +1,10 @@
 package com.cloudtech.sante;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by a.talviy on 27/07/13.
@@ -18,5 +20,21 @@ public class UserManagement extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.usermanagement, menu);
         return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_adduser:
+                Dialog dialog = new Dialog(this);
+                dialog.setContentView(R.layout.add_user);
+                dialog.setCancelable(true);
+                dialog.show();
+                break;
+            default:
+                break;
+        }
+        return super.onMenuItemSelected(featureId, item);
     }
 }
