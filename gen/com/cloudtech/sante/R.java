c737eee3c7126adca4b31969c1f9ede3d98cef8d
thannous <thannous@gmail.com>
2013-07-27 22:17:44 +0200

    splashscreen
    Signed-off-by: thannous <thannous@gmail.com>

--- gen/com/cloudtech/sante/R.java
+++ gen/com/cloudtech/sante/R.java
@@ -9,8 +9,8 @@ package com.cloudtech.sante;
 
 public final class R {
     public static final class array {
-        public static final int widgetListOptionsTimeLabels=0x7f050000;
-        public static final int widgetListOptionsTimeValues=0x7f050001;
+        public static final int widgetListOptionsTimeLabels=0x7f060000;
+        public static final int widgetListOptionsTimeValues=0x7f060001;
     }
     public static final class attr {
     }
@@ -21,39 +21,62 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int user_info=0x7f020001;
-        public static final int widget_bg=0x7f020002;
+        public static final int splash=0x7f020001;
+        public static final int user_info=0x7f020002;
+        public static final int widget_bg=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0007;
-        public static final int analogClock1=0x7f0a0004;
-        public static final int dashboard_grid=0x7f0a0000;
-        public static final int dashboard_icon_img=0x7f0a0001;
-        public static final int dashboard_icon_text=0x7f0a0002;
-        public static final int editText1=0x7f0a0003;
-        public static final int nap_time=0x7f0a0006;
-        public static final int widget_image=0x7f0a0005;
+        public static final int action_about=0x7f0b000f;
+        public static final int action_adduser=0x7f0b0010;
+        public static final int action_users=0x7f0b000e;
+        public static final int addUser=0x7f0b000b;
+        public static final int analogClock1=0x7f0b0007;
+        public static final int btn_back=0x7f0b000c;
+        public static final int btn_cancel=0x7f0b0002;
+        public static final int button_capture=0x7f0b0008;
+        public static final int camera_preview=0x7f0b0005;
+        public static final int dashboard_grid=0x7f0b0000;
+        public static final int dashboard_icon_img=0x7f0b0003;
+        public static final int dashboard_icon_text=0x7f0b0004;
+        public static final int menu_settings=0x7f0b000d;
+        public static final int nap_time=0x7f0b000a;
+        public static final int progressBar1=0x7f0b0001;
+        public static final int shutter_button=0x7f0b0006;
+        public static final int widget_image=0x7f0b0009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int dashboard_icon=0x7f030001;
-        public static final int document_layout=0x7f030002;
-        public static final int memo_layout=0x7f030003;
-        public static final int sante_widget_layout=0x7f030004;
+        public static final int activity_splash=0x7f030001;
+        public static final int add_user=0x7f030002;
+        public static final int dashboard_icon=0x7f030003;
+        public static final int document_layout=0x7f030004;
+        public static final int memo_layout=0x7f030005;
+        public static final int preview=0x7f030006;
+        public static final int sante_widget_layout=0x7f030007;
+        public static final int user_management=0x7f030008;
     }
     public static final class menu {
-        public static final int main=0x7f090000;
+        public static final int activity_camera=0x7f0a0000;
+        public static final int main=0x7f0a0001;
+        public static final int usermanagement=0x7f0a0002;
+    }
+    public static final class raw {
+        public static final int ormlite_config=0x7f050000;
     }
     public static final class string {
-        public static final int action_settings=0x7f070001;
-        public static final int app_name=0x7f070000;
-        public static final int hello_world=0x7f070002;
-        public static final int my_health_data=0x7f070003;
+        public static final int action_about=0x7f080007;
+        public static final int action_adduser=0x7f080005;
+        public static final int action_settings=0x7f080002;
+        public static final int action_users=0x7f080003;
+        public static final int app_name=0x7f080000;
+        public static final int hello_world=0x7f080004;
+        public static final int menu_settings=0x7f080001;
+        public static final int my_health_data=0x7f080006;
     }
     public static final class style {
         /** 
@@ -77,14 +100,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080003;
+        public static final int AppBaseTheme=0x7f090003;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080004;
-        public static final int Widget=0x7f080001;
-        public static final int Widget_Text=0x7f080002;
-        public static final int dashboard=0x7f080000;
+        public static final int AppTheme=0x7f090004;
+        public static final int Widget=0x7f090001;
+        public static final int Widget_Text=0x7f090002;
+        public static final int dashboard=0x7f090000;
     }
     public static final class xml {
         public static final int sante_widget_meta=0x7f040000;
