a141807e73805f8a8aaf0a165b36b98bcd3ffd59
thannous <thannous@gmail.com>
2013-07-27 17:39:55 +0200

    Ajout du menu
    Signed-off-by: thannous <thannous@gmail.com>

--- src/com/cloudtech/sante/MainActivity.java
+++ src/com/cloudtech/sante/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -144,4 +145,11 @@ public class MainActivity extends Activity implements OnItemClickListener {
 
 
 	}
+	  @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        // Inflate the menu; this adds items to the action bar if it is present.
+	        getMenuInflater().inflate(R.menu.main, menu);
+	        menu.findItem(R.id.action_users).setIntent(new Intent(this, UserManagement.class));
+	        return true;
+	    }
 }
