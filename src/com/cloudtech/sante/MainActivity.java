a9c1a5f3bbc9cf969c0583d4bd6bdc5559363372
jminouflet <orion@orion.cx>
2013-07-28 11:03:17 +0100

    adding credit page

--- src/com/cloudtech/sante/MainActivity.java
+++ src/com/cloudtech/sante/MainActivity.java
@@ -4,6 +4,7 @@ package com.cloudtech.sante;
 import com.cloudtech.sante.photo.DocumentActivity;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,134 +15,155 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity implements OnItemClickListener {
 
-	static final String EXTRA_MAP = "map";
+    static final String EXTRA_MAP = "map";
 
-	static final LauncherIcon[] ICONS = {
-			new LauncherIcon(R.drawable.doc, "Mes document",
-					"doc.png"),
-			new LauncherIcon(R.drawable.info, "Mes infos de sante",
-					"info.png"),
-			new LauncherIcon(R.drawable.urgence, "Urgence",
-					"urgence.png"),
-			new LauncherIcon(R.drawable.evo, "Mon evolution",
-					"evo.png"),
-			new LauncherIcon(R.drawable.memo, "Memo",
-					"memo.png"),
-			new LauncherIcon(R.drawable.time, "Timeline",
-					"time.png")};
+    static final LauncherIcon[] ICONS = {
+            new LauncherIcon(R.drawable.doc, "Mes document",
+                    "doc.png"),
+            new LauncherIcon(R.drawable.info, "Mes infos de sante",
+                    "info.png"),
+            new LauncherIcon(R.drawable.urgence, "Urgence",
+                    "urgence.png"),
+            new LauncherIcon(R.drawable.evo, "Mon evolution",
+                    "evo.png"),
+            new LauncherIcon(R.drawable.memo, "Memo",
+                    "memo.png"),
+            new LauncherIcon(R.drawable.time, "Timeline",
+                    "time.png")};
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		GridView gridview = (GridView) findViewById(R.id.dashboard_grid);
-		gridview.setAdapter(new ImageAdapter(this));
-		gridview.setOnItemClickListener(this);
+        GridView gridview = (GridView) findViewById(R.id.dashboard_grid);
+        gridview.setAdapter(new ImageAdapter(this));
+        gridview.setOnItemClickListener(this);
 
-		// Hack to disable GridView scrolling
-		gridview.setOnTouchListener(new OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				return event.getAction() == MotionEvent.ACTION_MOVE;
-			}
-		});
-	}
+        // Hack to disable GridView scrolling
+        gridview.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return event.getAction() == MotionEvent.ACTION_MOVE;
+            }
+        });
+    }
 
-	@Override
-	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-		Intent intent = null;
-		if(position ==0){
-			intent = new Intent(this, DocumentActivity.class);
-		}else if( position == 1){
-			intent = new Intent(this, InfoActivity.class);
-		}else if( position == 2){
-			intent = new Intent(this, UrgenceActivity.class);
-		}else if( position == 3){
-			 intent = new Intent(this, EvolutionActivity.class);
-		}else if( position == 4){
-			 intent = new Intent(this, MemoActivity.class);
-		}else if( position == 5){
-			 intent = new Intent(this, TimelineActivity.class);
-		}
+    @Override
+    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+        Intent intent = null;
+        if (position == 0) {
+            intent = new Intent(this, DocumentActivity.class);
+        } else if (position == 1) {
+            intent = new Intent(this, InfoActivity.class);
+        } else if (position == 2) {
+            intent = new Intent(this, UrgenceActivity.class);
+        } else if (position == 3) {
+            intent = new Intent(this, EvolutionActivity.class);
+        } else if (position == 4) {
+            intent = new Intent(this, MemoActivity.class);
+        } else if (position == 5) {
+            intent = new Intent(this, TimelineActivity.class);
+        }
 
-		intent.putExtra(EXTRA_MAP, ICONS[position].map);
-		startActivity(intent);
-	}
+        intent.putExtra(EXTRA_MAP, ICONS[position].map);
+        startActivity(intent);
+    }
 
-	static class LauncherIcon {
-		final String text;
-		final int imgId;
-		final String map;
+    static class LauncherIcon {
+        final String text;
+        final int imgId;
+        final String map;
 
-		public LauncherIcon(int imgId, String text, String map) {
-			super();
-			this.imgId = imgId;
-			this.text = text;
-			this.map = map;
-		}
+        public LauncherIcon(int imgId, String text, String map) {
+            super();
+            this.imgId = imgId;
+            this.text = text;
+            this.map = map;
+        }
 
-	}
+    }
 
-	static class ImageAdapter extends BaseAdapter {
-		private Context mContext;
+    static class ImageAdapter extends BaseAdapter {
+        private Context mContext;
 
-		public ImageAdapter(Context c) {
-			mContext = c;
-		}
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
 
-		@Override
-		public int getCount() {
-			return ICONS.length;
-		}
+        @Override
+        public int getCount() {
+            return ICONS.length;
+        }
 
-		@Override
-		public LauncherIcon getItem(int position) {
-			return null;
-		}
+        @Override
+        public LauncherIcon getItem(int position) {
+            return null;
+        }
 
-		@Override
-		public long getItemId(int position) {
-			return 0;
-		}
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
 
-		static class ViewHolder {
-			public ImageView icon;
-			public TextView text;
-		}
+        static class ViewHolder {
+            public ImageView icon;
+            public TextView text;
+        }
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View v = convertView;
-			ViewHolder holder;
-			if (v == null) {
-				LayoutInflater vi = (LayoutInflater) mContext
-						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            ViewHolder holder;
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater) mContext
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-				v = vi.inflate(R.layout.dashboard_icon, null);
-				holder = new ViewHolder();
-				//holder.text = (TextView) v
-				//		.findViewById(R.id.dashboard_icon_text);
-				holder.icon = (ImageView) v
-						.findViewById(R.id.dashboard_icon_img);
-				v.setTag(holder);
-			} else {
-				holder = (ViewHolder) v.getTag();
-			}
+                v = vi.inflate(R.layout.dashboard_icon, null);
+                holder = new ViewHolder();
+                //holder.text = (TextView) v
+                //		.findViewById(R.id.dashboard_icon_text);
+                holder.icon = (ImageView) v
+                        .findViewById(R.id.dashboard_icon_img);
+                v.setTag(holder);
+            } else {
+                holder = (ViewHolder) v.getTag();
+            }
 
-			holder.icon.setImageResource(ICONS[position].imgId);
-			//holder.text.setText(ICONS[position].text);
+            holder.icon.setImageResource(ICONS[position].imgId);
+            //holder.text.setText(ICONS[position].text);
 
-			return v;
-		}
+            return v;
+        }
 
 
-	}
-	  @Override
-	    public boolean onCreateOptionsMenu(Menu menu) {
-	        // Inflate the menu; this adds items to the action bar if it is present.
-	        getMenuInflater().inflate(R.menu.main, menu);
-	        menu.findItem(R.id.action_users).setIntent(new Intent(this, UserManagement.class));
-	        return true;
-	    }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        menu.findItem(R.id.action_users).setIntent(new Intent(this, UserManagement.class));
+        // pour le menu a propos, on ne fera pas de navigation vers une activity
+        // a la place on le gere la methode de selection de menu
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_about:
+                final Dialog dialog = new Dialog(this);
+                //dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setTitle(R.string.action_about);
+                dialog.setContentView(R.layout.credit);
+                dialog.setCancelable(true);
+                dialog.show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onMenuItemSelected(featureId, item);
+    }
 }
