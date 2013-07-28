d44ff1587f40c8ee620af3c7e8b8ed567e5f0e13
talanta <a.talviy@gmail.com>
2013-07-28 11:14:48 +0200

    mise à jour de la vue memo

--- src/com/cloudtech/sante/MemoActivity.java
+++ src/com/cloudtech/sante/MemoActivity.java
@@ -1,11 +1,17 @@
 package com.cloudtech.sante;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MemoActivity extends Activity{
-	   public void onCreate(final Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-     setContentView(R.layout.memo_layout);
-	   }
+public class MemoActivity extends Activity {
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memo_layout);
+
+        //ACTION BAR
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
 }
--- src/com/cloudtech/sante/UrgenceActivity.java
+++ src/com/cloudtech/sante/UrgenceActivity.java
@@ -4,15 +4,15 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class UrgenceActivity extends Activity{
-	   public void onCreate(final Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-     setContentView(R.layout.urgence_layout);
+public class UrgenceActivity extends Activity {
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.urgence_layout);
 
-           //ACTION BAR
-           ActionBar actionBar = getActionBar();
-           actionBar.setDisplayHomeAsUpEnabled(true);
+        //ACTION BAR
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
 
-       }
+    }
 }
