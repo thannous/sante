09776d4da16dd135dbb2c862f41f0188ddb39afc
talanta <a.talviy@gmail.com>
2013-07-28 10:32:07 +0200

    Mis a jour des vues urgences, timeline et mon evolution

--- src/com/cloudtech/sante/EvolutionActivity.java
+++ src/com/cloudtech/sante/EvolutionActivity.java
@@ -1,11 +1,17 @@
 package com.cloudtech.sante;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class EvolutionActivity extends Activity{
 	   public void onCreate(final Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-     setContentView(R.layout.document_layout);
-	   }
+           setContentView(R.layout.timeline_layout);
+
+           //ACTION BAR
+           ActionBar actionBar = getActionBar();
+           actionBar.setDisplayHomeAsUpEnabled(true);
+
+       }
 }
--- src/com/cloudtech/sante/UrgenceActivity.java
+++ src/com/cloudtech/sante/UrgenceActivity.java
@@ -1,11 +1,18 @@
 package com.cloudtech.sante;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class UrgenceActivity extends Activity{
 	   public void onCreate(final Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-     setContentView(R.layout.document_layout);
-	   }
+     setContentView(R.layout.urgence_layout);
+
+           //ACTION BAR
+           ActionBar actionBar = getActionBar();
+           actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+       }
 }
